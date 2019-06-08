80cc04e5ec4d0b25cbbbe8e6663052bce28df7ab
SimonHoworus <51452104+SimonHoworus@users.noreply.github.com>
2019-06-08 20:24:53 +0200

    ok5

--- myscript.js
+++ myscript.js
@@ -13,3 +13,16 @@ for (i = 0; i < dropdown.length; i++) {
   }
   });
 }
+window.onscroll = function() {scrollFunction()};
+function scrollFunction()
+ {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+        document.getElementById("myBtn").style.display = "block";
+    else
+        document.getElementById("myBtn").style.display = "none";
+    }
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
